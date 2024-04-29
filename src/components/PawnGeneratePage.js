136ead6239d6f77dbd92bb5a0469b7d7fe01004e
Pasindu Umayanga <122021632+PasinduUmayangaSenanayaka@users.noreply.github.com>
2024-04-29 15:58:48 +0530

    Update PawnGeneratePage.js

--- src/components/PawnGeneratePage.js
+++ src/components/PawnGeneratePage.js
@@ -1,35 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import './pawnGenerateStyles.css'; // Import the CSS file
 import { FaTrashAlt, FaPlus } from 'react-icons/fa'; // Import the trash and plus icons
+/* eslint-enable no-unused-vars */
 
 const PawnGeneratePage = () => {
-  const [items, setItems] = useState([{ itemName: '', itemsQuantity: '', totalWeight: '', netWeight: '' }]);
-  const [monetaryValues, setMonetaryValues] = useState([]);
-  const [loanApprovalValue, setLoanApprovalValue] = useState('');
-  const [totalQuantity, setTotalQuantity] = useState(0);
-  const [totalGrossWeight, setTotalGrossWeight] = useState(0);
-  const [totalNetWeight, setTotalNetWeight] = useState(0);
-  const [averageCarat, setAverageCarat] = useState(0);
-
-  const calculateMonetaryValues = () => {
-    // Your calculation logic here
-  };
-
-  const addItem = () => {
-    setItems([...items, { itemName: '', itemsQuantity: '', totalWeight: '', netWeight: '' }]);
-  };
-
-  const removeItem = (index) => {
-    const updatedItems = [...items];
-    updatedItems.splice(index, 1);
-    setItems(updatedItems);
-  };
-
-  const handleInputChange = (index, event, field) => {
-    const updatedItems = [...items];
-    updatedItems[index][field] = event.target.value;
-    setItems(updatedItems);
-  };
+  // Your component logic here
 
   return (
     <div className="container" style={{ backgroundColor: '#959191' }}>
