85cc404335927951d287f4970d5272e93bdeada6
Pasindu Umayanga <122021632+PasinduUmayangaSenanayaka@users.noreply.github.com>
2024-04-29 16:25:05 +0530

    Update PawnGeneratePage.js

--- src/components/PawnGeneratePage.js
+++ src/components/PawnGeneratePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FaTrashAlt, FaPlus } from 'react-icons/fa'; // Import the trash and plus icons
 
 const PawnGeneratePage = () => {
